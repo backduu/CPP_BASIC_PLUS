6834b657c962ed602d4fdee3116d57ec848d0762
backduu <power4290@gmail.com>
2023-11-10 10:08:32 +0900

    #1 CPP 시작 및 DOC 업뎃

---
+++ MyFirst/MyFirst/MyFirstProject.cpp
@@ -0,0 +1,11 @@
+#include <iostream>						// 선행 처리 지시자
+using namespace std;
+
+int main()								// 함수머리
+{										// 함수 몸체의 시작
+	cout << "welcome to c++ world!";	//
+	cout << endl;						// 새로운 행 시작
+	cout << "haha";				
+	return 0;							// main() 종료
+}										// 함수 몸체의 끝
+
